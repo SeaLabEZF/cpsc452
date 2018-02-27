3458def3db173dcf5784c138cf3b689cc2e98f7b
SeaLabEZF <sean.wulwick@gmail.com>
2018-02-27 11:57:05 -0800

    Main updated
    added the argv inputs, still needs testing on linux environment.

--- Assignment_1/PythonApplication1/main.py
+++ Assignment_1/PythonApplication1/main.py
@@ -1,17 +1,36 @@
+import sys
 import Ciphers
 
-plaintext = "transpositionciphers"
-key = "2135467"
-ciphertext = ''
-decrypted = ''
-
-testCase = Ciphers.Row_Transposition()
-
-testCase.setKey(key)
-ciphertext = testCase.encrypt(plaintext)
-decrypted = testCase.decrypt(ciphertext)
-print(plaintext)
-print(ciphertext)
-print(decrypted)
-
-
+if len(sys.argv) < 6:
+    print("You are missing arguments, check your input.")
+else:
+    if str(sys.argv[1]) == 'PLF':
+        cipher = Ciphers.Playfair
+    elif str(sys.argv[1]) == 'RTS':
+        cipher = Ciphers.Row_Transposition
+    elif str(sys.argv[1]) == 'RFC':
+        cipher = Ciphers.Railfence
+    elif str(sys.argv[1]) == 'VIG':
+        cipher = Ciphers.Vigenere
+    elif str(sys.argv[1]) == 'CES':
+        cipher = Ciphers.Caesar     
+    else:
+        print("Unrecognized cipher type, check your spelling and try again.")
+        exit()
+    if cipher.setKey(str(sys.argv[2])):
+        inFile = open(str(sys.argv[4]),"r")
+        inputText = inFile.read()
+        if str(sys.argv[3]) == 'ENC':
+            outputText = cipher.encrypt(inputText)
+        elif str(sys.argv[3] == 'DEC'):
+            outputText = cipher.decrypt(inputText)
+        else:
+            print("Unrecognized coding type, did you want Encode or Decode?")
+            exit()
+        outFile = open(str(sys.argv[5]), "w")
+        outFile.write(outputText)
+        inFile.close()
+        outFile.close()
+        print("Encryption Complete. Stored in ", str(sys.argv[5]))  
+    else:
+        print("Key is incorrect check to ensure your key matches with the cipher type")         
